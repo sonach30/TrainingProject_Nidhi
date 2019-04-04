88d70a033a0285de4ec932533f37e9040b41ae49
Nidhi Chaubey <nidhich30@gmail.com>
2019-04-05 01:21:48 +0530

    Added creation of cookie and stay signed functionality

--- src/com/tadigital/ecommerce/customer/servlet/CustomerLoginControllerServlet.java
+++ src/com/tadigital/ecommerce/customer/servlet/CustomerLoginControllerServlet.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,8 +19,10 @@ import com.tadigital.ecommerce.customer.service.CustomerService;
 public class CustomerLoginControllerServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
 		String email = req.getParameter("em");
 		String password = req.getParameter("pwd");
+		String signedIn = req.getParameter("ss");
 
 		// CALLING CUSTOMER CLASS IN ENTITY PACKAGE FOR SETTING VALUES FOR CUSTOMER
 		// FIRSTNAME LASTNAME EMAIL AND PASSWORD
@@ -27,26 +30,108 @@ public class CustomerLoginControllerServlet extends HttpServlet {
 		customer.setCust_email(email);
 		customer.setCust_password(password);
 
+		HttpSession session = req.getSession();
+
+		
 		// CALLING THE SERVICE METHOS TO GET STATUS FOR LOGIN
 		CustomerService customerService = new CustomerService();
 		boolean status = customerService.loginCustomer(customer);
-		
-		
-		HttpSession session = req.getSession();
+
+		//SETTING SESSION VARIABLE WITH THE CUSTOMER DETAILS
 		session.setAttribute("CUSTOMERDATA", customer);
+		
+		//ON SUCCESSFUL EXECUTION OF QUERY FOR LOGIN
 		if (status) {
-			HttpSession ses = req.getSession();
-			ses.setAttribute("SUCCESS", 2);
+			
+			//IF SIGNEDIN BOX CHECKED
+			if (signedIn != null) {
+				
+				// CREATE NEW COOKIE
+				long curLoginTime = System.currentTimeMillis();
+				String cValue = email + "-" + curLoginTime;
+				customer.setCust_lltime(curLoginTime);
+				Cookie cookie = new Cookie("abc", cValue);
+				cookie.setMaxAge(60 * 60 * 24 * 30);
+				res.addCookie(cookie);
+				
+				CustomerService service = new CustomerService();
+				boolean status1 = service.cookieLogin(customer);
 
-			RequestDispatcher rd = req.getRequestDispatcher("CustomerAccount.jsp");
-			rd.forward(req, res);
+				if (status1) {
+					
+					session.setAttribute("SUCCESS", "succesLogin");
+					RequestDispatcher rd = req.getRequestDispatcher("CustomerAccount.jsp");
+					
+					try {
+						
+						rd.forward(req, res);
+						
+					}catch(Exception e) {
+						
+						e.printStackTrace();
+					}
+					
+				} else {
+				
+					session.setAttribute("SUCCESS", "faillogin");
+					RequestDispatcher rd = req.getRequestDispatcher("SignInSignUpForms.jsp");
+					try {
+						
+						rd.forward(req, res);
+						
+					}catch(Exception e) {
+						
+						e.printStackTrace();
+					}
+				}
+
+				
+			} else {
+				
+				session.setAttribute("SUCCESS", "succesSlogin");
+				RequestDispatcher rd = req.getRequestDispatcher("CustomerAccount.jsp");
+				
+				try {
+					
+					rd.forward(req, res);
+					
+				}catch(Exception e) {
+					
+					e.printStackTrace();
+				}
+
+			}
 		} else {
-			HttpSession ses = req.getSession();
-			ses.setAttribute("SUCCESS", -2);
-
+			
+			session.setAttribute("SUCCESS", "faillogin");
 			RequestDispatcher rd = req.getRequestDispatcher("SignInSignUpForms.jsp");
-			rd.forward(req, res);
+			
+			try {
+				
+				rd.forward(req, res);
+				
+			}catch(Exception e) {
+				
+				e.printStackTrace();
+			}
 		}
 
+	}
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+		HttpSession session = req.getSession();
+		
+		String cValue = (String) session.getAttribute("COOKIEDATA");
+		String[] data = cValue.split("-");
+		String email = data[0];
+		long lastLoginTime = Long.parseLong(data[1]);
+
+		Customer customer = new Customer();
+		customer.setCust_lltime(lastLoginTime);
+		customer.setCust_email(email);
+
+		CustomerService customerservice = new CustomerService();
+		customerservice.directcookieLogin(customer); 
 	}
 }
