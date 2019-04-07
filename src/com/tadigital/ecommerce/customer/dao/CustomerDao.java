dc8093e6bc6a7d92473ab6986be9210d11167680
Nidhi Chaubey <nidhich30@gmail.com>
2019-04-07 23:59:57 +0530

    Remove the errors in updateCookie function query

--- src/com/tadigital/ecommerce/customer/dao/CustomerDao.java
+++ src/com/tadigital/ecommerce/customer/dao/CustomerDao.java
@@ -1,10 +1,9 @@
 package com.tadigital.ecommerce.customer.dao;
 
-
-	import java.sql.Connection;
-	import java.sql.ResultSet;
-	import java.sql.SQLException;
-	import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 	/*import java.util.ArrayList;
 	import java.util.Calendar;
 	import java.util.Date;
@@ -12,195 +11,191 @@ package com.tadigital.ecommerce.customer.dao;
 */
 import com.tadigital.ecommerce.customer.entity.Customer;
 
-	public class CustomerDao extends Dao {
+public class CustomerDao extends Dao {
+	
+	//EXTRACTING AND ASSIGNING DETAILS FROM customer_details TABLE FOR ENTERED EMAIL AND PASSWORD
+	public boolean selectCustomerByEmailAndPassword(Customer customer) {
+
+		boolean status = false;
 		
-		//EXTRACTING AND ASSIGNING DETAILS FROM customer_details TABLE FOR ENTERED EMAIL AND PASSWORD
-		public boolean selectCustomerByEmailAndPassword(Customer customer) {
-			boolean status = false;
+		//ESTABLISHING DATABSE CONNECTION
+		Connection con = openConnection();
+		Statement stmt = openStatement(con);
+		ResultSet rs = null;
+		
+		try {
+			String sql = "SELECT * FROM customer_details WHERE cust_email = '" + customer.getCust_email() + "' AND cust_password = '" + customer.getCust_password() + "'";
+			rs = stmt.executeQuery(sql);
 			
-			//ESTABLISHING DATABSE CONNECTION
-			Connection con = openConnection();
-			Statement stmt = openStatement(con);
-			ResultSet rs = null;
-			
-			try {
-				String sql = "SELECT * FROM customer_details WHERE cust_email = '" + customer.getCust_email() + "' AND cust_password = '" + customer.getCust_password() + "'";
-				rs = stmt.executeQuery(sql);
+			//SETTING VALUES FOR CUSTOMER ID FIRSTNAME AND LASTNAME 
+			if(rs.next()) {
+				status = true;
 				
-				//SETTING VALUES FOR CUSTOMER ID FIRSTNAME AND LASTNAME 
-				if(rs.next()) {
-					status = true;
-					
-					//SETTING THE VALUES IN CUSTOMER ENTITY FROM THE DATABASE FOR CURRENT USER
-					customer.setCust_id(rs.getInt(1));
-					customer.setCust_fname(rs.getString(2));
-					customer.setCust_lname(rs.getString(3));
-					
-				}
+				//SETTING THE VALUES IN CUSTOMER ENTITY FROM THE DATABASE FOR CURRENT USER
+				customer.setCust_id(rs.getInt(1));
+				customer.setCust_fname(rs.getString(2));
+				customer.setCust_lname(rs.getString(3));
 				
-			} catch (SQLException sqle) {
-				sqle.printStackTrace();
-			} finally {
-				
-				//CLOSING THE STATEMENT,RESULTSET AND CONNECTION 
-				closeStatement(stmt);
-				closeResultSet(rs);
-				closeConnection(con);
 			}
 			
-			//RETURNING SELECT QUERY EXECUTION STATUS
-			return status;
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		} finally {
+			
+			//CLOSING THE STATEMENT,RESULTSET AND CONNECTION 
+			closeStatement(stmt);
+			closeResultSet(rs);
+			closeConnection(con);
 		}
 		
+		//RETURNING SELECT QUERY EXECUTION STATUS
+		return status;
+	}
+	
+	
+	//FUNCTION FOR INSERTING IN THE customer_details(First Name, Last Name, Email, Password)
+	//RETURN THE STATUS FOR INSERTING QUERY
+	public boolean insertCustomer(Customer customer) {
+		boolean status = false;
 		
+		//ESTABLISHING CONNECTION
+		Connection con = openConnection();
+		Statement stmt = openStatement(con);
 		
-		
-		//FUNCTION FOR INSERTING IN THE customer_details(First Name, Last Name, Email, Password)
-		//RETURN THE STATUS FOR INSERTING QUERY
-		public boolean insertCustomer(Customer customer) {
-			boolean status = false;
+		try {
+			String sql = "INSERT INTO customer_details(cust_fname, cust_lname, cust_email, cust_password) VALUES('" + customer.getCust_fname() + "', '" + customer.getCust_lname() + "', '" + customer.getCust_email() + "', '" + customer.getCust_password() + "')";
 			
-			//ESTABLISHING CONNECTION
-			Connection con = openConnection();
-			Statement stmt = openStatement(con);
+			//EXECUTING INSERT QUERY
+			int rows = stmt.executeUpdate(sql);
+			if(rows != 0) {
+				status = true;
+			}
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		} finally {
 			
-			try {
-				String sql = "INSERT INTO customer_details(cust_fname, cust_lname, cust_email, cust_password) VALUES('" + customer.getCust_fname() + "', '" + customer.getCust_lname() + "', '" + customer.getCust_email() + "', '" + customer.getCust_password() + "')";
+			//CLOSING THE DATABASE CONNECTION 
+			closeStatement(stmt);
+			closeConnection(con);
+		}
+		
+		//RETURNING THE INSERT QUERY EXECUTION STATUS 
+		return status;
+	}
+	
+	
+	//SETTING THE CUSTOMER DETAILS
+	public boolean updateCustomerById(Customer customer) {
+		boolean status = false;
+		Connection con = openConnection();
+		Statement stmt = openStatement(con);
+		ResultSet rs = null;
+		
+		try {
+			
+			String sql = "UPDATE customer_details SET cust_gender = '" + customer.getCust_gender() +"', cust_address='" +customer.getCust_address()+"', cust_city='" +customer.getCust_city()+"',  cust_zip='"+customer.getCust_zip()+"',cust_state='"+customer.getCust_state()+"' ,cust_country='"+customer.getCust_country()+"',cust_number='"+customer.getCust_number()+"' WHERE cust_id='"+customer.getCust_id()+"'";
+			int row = stmt.executeUpdate(sql);
+			if(row!=0) {
+				status = true;	
+			}
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		} finally {
+			closeStatement(stmt);
+			closeResultSet(rs);
+			closeConnection(con);
+		  }
+		
+		return status;
+	}
+	
+	//CHANGING THE PASSWORD
+	public boolean updateCustomerPasswordById(Customer customer) {
+		boolean status = false;
+		
+		Connection con = openConnection();
+		Statement stmt = openStatement(con);
+		
+		try {
+			
+			//UPDATE customer_information SET cust_gender=customer.getGender(), cust_address=customer.getAddress(), cust_city=customer.getCity,  cust_zip=customer.getZip(),
+			//cust_state=customer.getState(), cust_country=customer.getCountry
+			String sql = "UPDATE customer_details SET cust_password = '" + customer.getCust_password() +"' WHERE cust_id='"+customer.getCust_id()+"'";
+			int row = stmt.executeUpdate(sql);
+		
+			if(row!=0) {
+				status = true;
 				
-				//EXECUTING INSERT QUERY
-				int rows = stmt.executeUpdate(sql);
-				if(rows != 0) {
-					status = true;
-				}
-			} catch (SQLException sqle) {
-				sqle.printStackTrace();
-			} finally {
+			}
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		} finally {
+			closeStatement(stmt);
+			closeConnection(con);
+		}
+		
+		return status;
+	}
+	
+	
+	
+	//SETTING COOKIE END TIME
+	public boolean selectCookie(Customer customer) {
+		
+		boolean status = false;
+		
+		Connection con = openConnection();
+		Statement stmt = openStatement(con);
+		
+		try {	
+			//UPDATE customer_information SET cust_gender=customer.getGender(), cust_address=customer.getAddress(), cust_city=customer.getCity,  cust_zip=customer.getZip(),
+			//cust_state=customer.getState(), cust_country=customer.getCountry
+			String sql = "SELECT * FROM customer_details WHERE cust_email = '" + customer.getCust_email() + "' AND cust_password = '" + customer.getCust_password() + "'";
+		    int row = stmt.executeUpdate(sql);
+			
+			if(row!=0) {
 				
-				//CLOSING THE DATABASE CONNECTION 
-				closeStatement(stmt);
-				closeConnection(con);
+				status = true;	
 			}
 			
-			//RETURNING THE INSERT QUERY EXECUTION STATUS 
-			return status;
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		} finally {
+			closeStatement(stmt);
+			closeConnection(con);
 		}
 		
-		
-		
-		//SETTING THE CUSTOMER DETAILS
-		public boolean updateCustomerById(Customer customer) {
-			boolean status = false;
-			Connection con = openConnection();
-			Statement stmt = openStatement(con);
-			ResultSet rs = null;
-			
-			try {
-				
-				String sql = "UPDATE customer_details SET cust_gender = '" + customer.getCust_gender() +"', cust_address='" +customer.getCust_address()+"', cust_city='" +customer.getCust_city()+"',  cust_zip='"+customer.getCust_zip()+"',cust_state='"+customer.getCust_state()+"' ,cust_country='"+customer.getCust_country()+"',cust_number='"+customer.getCust_number()+"' WHERE cust_id='"+customer.getCust_id()+"'";
-				int row = stmt.executeUpdate(sql);
-				if(row!=0) {
-					status = true;	
-				}
-			} catch (SQLException sqle) {
-				sqle.printStackTrace();
-			} finally {
-				closeStatement(stmt);
-				closeResultSet(rs);
-				closeConnection(con);
-			  }
-			
-			return status;
-		}
-		
-		
-		
-		//CHANGING THE PASSWORD
-		public boolean updateCustomerPasswordById(Customer customer) {
-			boolean status = false;
-			
-			Connection con = openConnection();
-			Statement stmt = openStatement(con);
-			
-			try {
-				
-				//UPDATE customer_information SET cust_gender=customer.getGender(), cust_address=customer.getAddress(), cust_city=customer.getCity,  cust_zip=customer.getZip(),
-				//cust_state=customer.getState(), cust_country=customer.getCountry
-				String sql = "UPDATE customer_details SET cust_password = '" + customer.getCust_password() +"' WHERE cust_id='"+customer.getCust_id()+"'";
-				int row = stmt.executeUpdate(sql);
-			
-				if(row!=0) {
-					status = true;
-					
-				}
-			} catch (SQLException sqle) {
-				sqle.printStackTrace();
-			} finally {
-				closeStatement(stmt);
-				closeConnection(con);
-			}
-			
-			return status;
-		}
-		
-		
-		
-		//SETTING COOKIE END TIME
-		public boolean selectCookie(Customer customer) {
-			
-			boolean status = false;
-			
-			Connection con = openConnection();
-			Statement stmt = openStatement(con);
-			
-			try {	
-				//UPDATE customer_information SET cust_gender=customer.getGender(), cust_address=customer.getAddress(), cust_city=customer.getCity,  cust_zip=customer.getZip(),
-				//cust_state=customer.getState(), cust_country=customer.getCountry
-				String sql = "SELECT * FROM user_information WHERE usr_email=? and usr_lltime=?";
-			    int row = stmt.executeUpdate(sql);
-				
-				if(row!=0) {
-					
-					status = true;	
-				}
-				
-			} catch (SQLException sqle) {
-				sqle.printStackTrace();
-			} finally {
-				closeStatement(stmt);
-				closeConnection(con);
-			}
-			
-			return status;
-		}
-		
+		return status;
+	}
 	
 
-		//SELECT QUERY FOR COOKIE
-		public boolean updateCookieTime(Customer customer) {
-			boolean status = false;
+
+	//SELECT QUERY FOR COOKIE
+	public boolean updateCookieTime(Customer customer) {
+		boolean status = false;
+		
+		Connection con = openConnection();
+		Statement stmt = openStatement(con);
+		
+		try {
 			
-			Connection con = openConnection();
-			Statement stmt = openStatement(con);
+			//UPDATE customer_information SET cust_gender=customer.getGender(), cust_address=customer.getAddress(), cust_city=customer.getCity,  cust_zip=customer.getZip(),
+			//cust_state=customer.getState(), cust_country=customer.getCountry
+			String sql = "UPDATE customer_details SET cust_lltime = '" + customer.getCust_lltime() +"' WHERE cust_email='"+customer.getCust_email()+"'";
+			int row = stmt.executeUpdate(sql);
 			
-			try {
+			if(row!=0) {
 				
-				//UPDATE customer_information SET cust_gender=customer.getGender(), cust_address=customer.getAddress(), cust_city=customer.getCity,  cust_zip=customer.getZip(),
-				//cust_state=customer.getState(), cust_country=customer.getCountry
-				String sql = "UPDATE customer_details SET cust_lltime = '" + customer.getCust_lltime() +"' WHERE cust_email='"+customer.getCust_email()+"'";
-				int row = stmt.executeUpdate(sql);
+				status = true;
 				
-				if(row!=0) {
-					
-					status = true;
-					
-				}
-			} catch (SQLException sqle) {
-				sqle.printStackTrace();
-			} finally {
-				closeStatement(stmt);
-				closeConnection(con);
 			}
-			
-			return status;
-		}		
-	}
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		} finally {
+			closeStatement(stmt);
+			closeConnection(con);
+		}
+		
+		return status;
+	}		
+}
