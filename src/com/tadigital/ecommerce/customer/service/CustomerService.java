720b41810df89fe974f4ee269e2041c647ee4f27
sonach30 <38238655+sonach30@users.noreply.github.com>
2019-04-08 00:41:10 +0530

    Update CustomerService.java

--- src/com/tadigital/ecommerce/customer/service/CustomerService.java
+++ src/com/tadigital/ecommerce/customer/service/CustomerService.java
@@ -57,78 +57,78 @@ public class CustomerService {
 	// SENDING MAIL ON SUCCESSFUL RESGISTRATION
 	public String sendRegisterMail(String name, String email) {
 			
-			String status = "NOT SENT";
+		String status = "NOT SENT";
 
-			// MAIL SERVER CONFIGURATION
-			Properties properties = new Properties();
-			properties.put("mail.smtp.host", "smtp.gmail.com");
-			properties.put("mail.smtp.socketFactory.port", "465");
-			properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-			properties.put("mail.smtp.auth", "true");
-			properties.put("mail.smtp.port", "465");
-			
-			//MAIL LOGIN CONFIGURATION
-			InputStream inputStream = getClass().getResourceAsStream("Email.properties");
-			try {
-				properties.load(inputStream);
-			} catch (IOException ioe) {
-				ioe.printStackTrace();
+		// MAIL SERVER CONFIGURATION
+		Properties properties = new Properties();
+		properties.put("mail.smtp.host", "smtp.gmail.com");
+		properties.put("mail.smtp.socketFactory.port", "465");
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.port", "465");
+
+		//MAIL LOGIN CONFIGURATION
+		InputStream inputStream = getClass().getResourceAsStream("Email.properties");
+		try {
+			properties.load(inputStream);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+		String id = properties.getProperty("email_id");
+		String password = properties.getProperty("email_password");
+
+		// CONNECT TO MAIL SERVER
+		Session session = Session.getDefaultInstance(properties, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+
+				return new PasswordAuthentication(id,password);
 			}
-			String id = properties.getProperty("email_id");
-			String password = properties.getProperty("email_password");
+		});
 
-			// CONNECT TO MAIL SERVER
-			Session session = Session.getDefaultInstance(properties, new javax.mail.Authenticator() {
-				protected PasswordAuthentication getPasswordAuthentication() {
-					
-					return new PasswordAuthentication(id,password);
-				}
-			});
-			
-			try {
-				// COMPOSE MESSAGE
-				MimeMessage mimeMessage = new MimeMessage(session);
-				mimeMessage.setFrom(new InternetAddress(id));
-				mimeMessage.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
-				
-				MimeMultipart mp = new MimeMultipart();
-				MimeBodyPart mbp1 = new MimeBodyPart();
-				
-				String msg = "Hi,<body bgcolor='white'>Dear <i>" + name + "</font></i>,<br/><br/>"
-						+ "<img src='cid:image1'><br/><h2><font color='blue' font-family='Comic Sans MS'><b>Welcome to the our Family!!</b></font></h2>"
-						+ "<br/><font color='cyan'>We are to glad to you have you on-board</font>"
-						+ "<br/><br/><br/><font color='green'>Keep Updated by visiting the webpage regularly for new offers and discounts</font>"
-						+ "<br/><br/><br/><font color='red'>Happy Shopping!!!<br/>TA Digital<br/><br/></font><img src='cid:image'></body>";
-				
-				mbp1.setContent(msg, "text/html");
-				mp.addBodyPart(mbp1);
+		try {
+			// COMPOSE MESSAGE
+			MimeMessage mimeMessage = new MimeMessage(session);
+			mimeMessage.setFrom(new InternetAddress(id));
+			mimeMessage.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
 
-				mbp1 = new MimeBodyPart();
-				DataSource fds = new FileDataSource(
-						"D:/Trainee Engineer March 2019/workspace/TrainingProject_Nidhi/WebContent/images/logo.jpg");
-				
-				mbp1.setDataHandler(new DataHandler(fds));
-				mbp1.setHeader("Content-ID", "<image>");
-				mp.addBodyPart(mbp1);
+			MimeMultipart mp = new MimeMultipart();
+			MimeBodyPart mbp1 = new MimeBodyPart();
 
-				mbp1 = new MimeBodyPart();
-				fds = new FileDataSource(
-						"D:/Trainee Engineer March 2019/workspace/TrainingProject_Nidhi/WebContent/images/WELCOME.jpg");
-				mbp1.setDataHandler(new DataHandler(fds));
-				mbp1.setHeader("Content-ID", "<image1>");
-				mp.addBodyPart(mbp1);
+			String msg = "Hi,<body bgcolor='white'>Dear <i>" + name + "</font></i>,<br/><br/>"
+					+ "<img src='cid:image1'><br/><h2><font color='blue' font-family='Comic Sans MS'><b>Welcome to the our Family!!</b></font></h2>"
+					+ "<br/><font color='cyan'>We are to glad to you have you on-board</font>"
+					+ "<br/><br/><br/><font color='green'>Keep Updated by visiting the webpage regularly for new offers and discounts</font>"
+					+ "<br/><br/><br/><font color='red'>Happy Shopping!!!<br/>TA Digital<br/><br/></font><img src='cid:image'></body>";
 
-				mimeMessage.setContent(mp);
+			mbp1.setContent(msg, "text/html");
+			mp.addBodyPart(mbp1);
 
-				// SEND MAIL
-				Transport.send(mimeMessage);
+			mbp1 = new MimeBodyPart();
+			DataSource fds = new FileDataSource(
+					"D:/Trainee Engineer March 2019/workspace/TrainingProject_Nidhi/WebContent/images/logo.jpg");
 
-				status = "SENT";
-			} catch (MessagingException mex) {
-				mex.printStackTrace();
-			}
+			mbp1.setDataHandler(new DataHandler(fds));
+			mbp1.setHeader("Content-ID", "<image>");
+			mp.addBodyPart(mbp1);
 
-			return status;
+			mbp1 = new MimeBodyPart();
+			fds = new FileDataSource(
+					"D:/Trainee Engineer March 2019/workspace/TrainingProject_Nidhi/WebContent/images/WELCOME.jpg");
+			mbp1.setDataHandler(new DataHandler(fds));
+			mbp1.setHeader("Content-ID", "<image1>");
+			mp.addBodyPart(mbp1);
+
+			mimeMessage.setContent(mp);
+
+			// SEND MAIL
+			Transport.send(mimeMessage);
+
+			status = "SENT";
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+		}
+
+		return status;
 	}
 		
 		
